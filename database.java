1d9913f53202dc4ad01c27d72da68f37dece7e13
Helin Devani <133775761+helindevani@users.noreply.github.com>
2024-03-03 23:12:47 +0530

    Update database.java

--- database.java
+++ database.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package com.mycompany.loginwebapplication;
 
 import jakarta.jms.Connection;
